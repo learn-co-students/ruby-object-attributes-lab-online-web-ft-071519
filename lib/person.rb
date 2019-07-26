d289d974a63b2db9da10a2de0bc30951a8bcaabb
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-26 18:04:33 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  def intialize(person)
+    @name = person
+  end 
+  def name 
+    @name 
+  end 
+  def name=(new_person)
+    @name = new_person
+  end 
+  def job 
+    @job
+  end 
+  def job=(career)
+    @job = career
+  end
+end
+
+  
